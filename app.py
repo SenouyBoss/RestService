10ffb3da5ad395d4aec2d7ed2236b6cdb8b8ea8e
Younes <xx.senouy.xx@gmail.com>
2020-07-15 14:13:27 +0100

    Add files via upload
    project skeleton not ready for deployment yet

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import jsonify, Flask, request
+import requests
+import json
+
+app = Flask(__name__)
+
+r = requests.get(
+        'https://api.github.com/search/repositories?q=created:<=2020-07-13&sort=stars&order=desc&per_page=100')
+data = r.json()
+#items = data['items']
+#data1 = data['items']
+#data2 = json.loads(data1)
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify(data)
+
+@app.route('/A1count', methods=['GET'])
+def repos_count_per_lang():
+    languages = {}
+    for x in range(100):
+        num = 0
+        for y in range(100):
+            if (data['items'][x]['language'] == data['items'][y]['language']):
+                num = num + 1
+                language = {data['items'][x]['language']: num}
+        languages.update(language)
+    return '<h1>Number of repos using each language is : </br></h1>' + json.dumps(languages, indent=4)
+
+@app.route('/A1list', methods=['GET'])
+def repos_list_per_lang():
+    reposdict = {}
+    for x in range(100):
+         reposList = []
+         for y in range(100):
+             if (data['items'][x]['language'] == data['items'][y]['language']):
+                name = data['items'][y]['full_name']
+                reposList.append(name)
+                repos = {data['items'][x]['language']: reposList}
+         reposdict.update(repos)
+
+    return '<h1></br>List of repos using each language is : </br></h1>' + json.dumps(reposdict)
